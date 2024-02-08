fe87d0ed9ce67ec3f1f69f1a4f894f91f8a99182
aycom366 <bamigboyeayomide200@gmail.com>
2024-02-08 17:14:10 +0100

    simulated error with msw

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Users } from "./Users";
+import { server } from "../../mocks/server";
+import { rest } from "msw";
 
 describe("Mocking HttP requests", () => {
   it("should render the component correctly", () => {
@@ -12,5 +14,21 @@ describe("Mocking HttP requests", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  it("renders error message when fetch fails", async () => {
+    // Override the default request handler to return a 500 status code.
+    //This is resetting the default request handler for the URL to return a 500 status code.
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<Users />);
+    const errorMessage = await screen.findByText("Error fetching users");
+    expect(errorMessage).toBeInTheDocument();
   });
 });
